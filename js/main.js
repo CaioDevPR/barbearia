eec99b458c18fd52e629c347d49d834d5624d9dd
CaioDevPR <programadorpr@outlook.com>
2023-03-16 20:00:36 -0300

    auto play slider  com js

--- js/main.js
+++ js/main.js
@@ -9,4 +9,20 @@ function closeMenu(){
 }
 
 
+/* SLIDER AUTO */
 
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+nextImage();
+}, 2000)
+
+function nextImage(){
+    count++;
+    if(count>3){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true; 
+}
